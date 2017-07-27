e2af8a86e87bc2ee0fad6b75998a17e9a1c94b27
GabbasovDinar <gabbasov@it-projects.info>
2017-07-27 15:08:42 +0500

    [PORT 27 jule]

--- project_timelog/__openerp__.py
+++ project_timelog/__openerp__.py
@@ -3,29 +3,39 @@
     "name": """Time Tracker""",
     "summary": """Adds Start/Stop buttons to task work lines. Allows to see statistics on Calendar, Graph, Tree views and more""",
     "category": "Project",
+    "images": ["images/timelog.png"],
     "version": "1.0.0",
-    "images": ['images/timelog.png'],
+    "application": False,
+
     "author": "IT-Projects LLC, Dinar Gabbasov",
-    'website': "https://twitter.com/gabbasov_dinar",
+    "website": "https://twitter.com/gabbasov_dinar",
     "license": "GPL-3",
-    "price": 390,
+    "price": 390.00,
     "currency": "EUR",
 
     "depends": [
-        "bus",
-        "im_chat",
-        "base_action_rule",
         "project_timesheet",
+        "base_action_rule",
+        "mail_base",
     ],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
-        "security/ir.model.access.csv",
-        "views/project_timelog_views.xml",
-        "views/res_config_view.xml",
-        "views/project_timelog_templates.xml",
-        "data/project_timelog_data.xml",
-        "data/pre_install.yml",
+        # "security/ir.model.access.csv",
+        # "views/project_timelog_views.xml",
+        # "views/res_config_view.xml",
+        # "views/project_timelog_templates.xml",
+        # "data/project_timelog_data.xml",
+        # "data/pre_install.yml",
+    ],
+    "qweb": [
+    ],
+    "demo": [
     ],
 
-    "installable": False,
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
 }
